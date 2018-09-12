fd93539778c8643409859c6c434d5b8b49287a88
Анастасия <aasya9999@gmail.com>
2018-09-13 01:49:53 +0500

    week3

---
+++ week4/ex2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h> 
+#include <unistd.h>
+
+
+int main(void) {
+  int i;
+  for (i = 0; i < 3; i++) {
+      fork();
+      sleep(5); 
+      
+  }
+  return 0;
+}
